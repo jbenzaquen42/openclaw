8352abc11f31781c68ec0abdc774979eb8c526f1
jbenzaquen42 <jacobbenzaquen53@gmail.com>
2026-03-26 17:18:02 -0500

    REMOVE ALL EXCEPT DOCS

--- extensions/memory-core/src/tools.runtime.ts
+++
@@ -1,5 +0,0 @@
-export {
-  getMemorySearchManager,
-  readAgentMemoryFile,
-  resolveMemoryBackendConfig,
-} from "openclaw/plugin-sdk/memory-core";
--- src/plugin-sdk/speech.ts
+++
@@ -1,4 +0,0 @@
-// Public speech helpers for bundled or third-party plugins.
-
-export { parseTtsDirectives } from "../tts/tts-core.js";
-export type { SpeechVoiceOption } from "../tts/provider-types.js";
